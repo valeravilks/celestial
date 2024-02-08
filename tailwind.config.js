a3e7cc5e9acaa9ae227fe893e70c4b5f307e4e0a
Valera-V <valera@dreamersofday.co>
2024-02-09 00:52:42 +0300

    feat: add daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
 /** @type {import('tailwindcss').Config} config */
 const config = {
-  content: ['./index.php', './app/**/*.php', './resources/**/*.{php,vue,js}'],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#0003ff',
-        secondary: '#C90000',
-        white: '#ffffff',
-        black: '#000000',
-        light: '#F6F5F5',
-        dark: '#361515',
-        gray: '#D9D9D9',
-        lightgray: '#BBBBBB',
-      }, // Extend Tailwind's default colors
+    content: ['./index.php', './app/**/*.php', './resources/**/*.{php,vue,js}'],
+// add daisyUI plugin
+    plugins: [require("daisyui")],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#0003ff',
+                secondary: '#C90000',
+                white: '#ffffff',
+                black: '#000000',
+                light: '#F6F5F5',
+                dark: '#361515',
+                gray: '#D9D9D9',
+                lightgray: '#BBBBBB',
+            }, // Extend Tailwind's default colors
+        },
     },
-  },
-  plugins: [],
 };
 
 export default config;
