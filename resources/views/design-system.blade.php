630f4706687aa885f75f2c07fc303d612f1b95c3
Valera-V <valera@dreamersofday.co>
2024-02-09 22:26:28 +0300

    feat: add title and link in design system

--- resources/views/design-system.blade.php
+++ resources/views/design-system.blade.php
@@ -22,4 +22,14 @@
     </div>
     <x-atoms.docs-commets.index :class="App\View\Components\Atoms\Headers::class"/>
   </div>
+  <hr>
+  <x-atoms.title.h1>Title 1</x-atoms.title.h1>
+  <x-atoms.title.h2>Title 2</x-atoms.title.h2>
+  <x-atoms.title.h3>Title 3</x-atoms.title.h3>
+  <x-atoms.title.h4>Title 4</x-atoms.title.h4>
+  <x-atoms.title.h5>Title 5</x-atoms.title.h5>
+  <x-atoms.title.h6>Title 6</x-atoms.title.h6>
+  <hr>
+  <x-atoms.link.fill>Link Fill</x-atoms.link.fill>
+  <x-atoms.link.outline>Link Outline</x-atoms.link.outline>
 </div>
