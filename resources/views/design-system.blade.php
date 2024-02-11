e172251e17d8bcaa6b965bf6ed4b97025acec519
Valera-V <valera@dreamersofday.co>
2024-02-11 22:54:21 +0300

    feat: add design page use route

--- resources/views/design-system.blade.php
+++ resources/views/design-system.blade.php
@@ -1,35 +1,20 @@
-{{--
-  Template Name: Design System
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php wp_head(); ?>
+</head>
 
-  TODO: Create rooting for this page
---}}
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<?php do_action('get_header'); ?>
 
-{{--@extends('layouts.app')--}}
-
-<div class="container mx-auto">
-  <div class="grid grid-cols-2 gap-2">
-    <x-atoms.example/>
-    <x-atoms.docs-commets.index :class="App\View\Components\Atoms\Example::class"/>
-  </div>
-  <hr>
-  <div class="grid grid-cols-2 gap-2">
-    <div class="">
-      @foreach($mockHeaders as $text)
-        <x-atoms.headers>
-          {{ $text }}
-        </x-atoms.headers>
-      @endforeach
-    </div>
-    <x-atoms.docs-commets.index :class="App\View\Components\Atoms\Headers::class"/>
-  </div>
-  <hr>
-  <x-atoms.title.h1>Title 1</x-atoms.title.h1>
-  <x-atoms.title.h2>Title 2</x-atoms.title.h2>
-  <x-atoms.title.h3>Title 3</x-atoms.title.h3>
-  <x-atoms.title.h4>Title 4</x-atoms.title.h4>
-  <x-atoms.title.h5>Title 5</x-atoms.title.h5>
-  <x-atoms.title.h6>Title 6</x-atoms.title.h6>
-  <hr>
-  <x-atoms.link.fill>Link Fill</x-atoms.link.fill>
-  <x-atoms.link.outline>Link Outline</x-atoms.link.outline>
+<div id="app">
+  123
 </div>
+
+<?php do_action('get_footer'); ?>
+<?php wp_footer(); ?>
+</body>
+</html>
