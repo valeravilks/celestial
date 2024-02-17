265098ad48cd8ebe5dbb505daac3658f525f6ced
Valera-V <valera@dreamersofday.co>
2024-02-17 22:42:11 +0300

    feat: create "one column" atom

---
+++ resources/views/components/atoms/layout/one-column.blade.php
@@ -0,0 +1,9 @@
+<x-atoms.skip-to-content/>
+
+<div class="grid grid-rows-[auto_1fr_auto] min-h-screen">
+  @yield('header')
+  <main id="main">
+    @yield('main')
+  </main>
+  @yield('footer')
+</div>
