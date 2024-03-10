2f67511f83d82cb7ee41b59569eac93f0348898a
Valera-V <valera@dreamersofday.co>
2024-03-10 13:27:13 +0300

    feat: change color logo

--- resources/views/components/organisms/header/type1.blade.php
+++ resources/views/components/organisms/header/type1.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-primary py-2">
   <x-atoms.container>
     <div {{ $attributes->merge(['class' => 'flex items-center']) }}>
-      <x-molecules.logo.light class="grow"/>
+      <x-molecules.logo.light class="grow text-white"/>
       <x-molecules.menus.type/>
     </div>
   </x-atoms.container>
