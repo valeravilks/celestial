022e5ad8e9fbb41d6cb48910e404e0e8c20cec22
Valera-V <valera@dreamersofday.co>
2024-03-13 01:42:38 +0300

    feat: add blade icons

---
+++ config/blade-icons.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'sets' => [
+        'default' => [
+            'path' => '/resources/images/icons', # Relative path to the new directory
+            'prefix' => 'icon',
+        ],
+    ],
+];
--- resources/views/components/molecules/logo/light.blade.php
+++ resources/views/components/molecules/logo/light.blade.php
@@ -1,3 +1,5 @@
 <x-atoms.link.simple  {{ $attributes->merge(['href' => $url]) }}>
   {{ $text }}
 </x-atoms.link.simple>
+@svg("logo")
+@svg("logo/arrow")
