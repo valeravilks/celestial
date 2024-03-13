51bf2ceeb02f0943ec602b8c15ee9990d43846b4
Valera-V <valera@dreamersofday.co>
2024-03-13 23:54:39 +0300

    feat: add log1x/svg

--- resources/views/components/molecules/logo/light.blade.php
+++ resources/views/components/molecules/logo/light.blade.php
@@ -1,5 +1,5 @@
 <x-atoms.link.simple  {{ $attributes->merge(['href' => $url]) }}>
   {{ $text }}
 </x-atoms.link.simple>
-@svg("logo")
-@svg("logo/arrow")
+@svg("images.icons.logo")
+{{--@svg("logo/arrow")--}}
