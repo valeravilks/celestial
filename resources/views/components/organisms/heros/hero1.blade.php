3aa18caa1323cabaaf7a81608d476ff8431c2c52
dreamersvalera <valera@dreamersofday.co>
2024-07-21 01:16:00 +0300

    feat: add hero1

---
+++ resources/views/components/organisms/heros/hero1.blade.php
@@ -0,0 +1,7 @@
+<div data-component="{{ $componentName }}" {{ $attributes->merge(['class' => '']) }}>
+  <x-atoms.containers.container1>
+    <x-atoms.texts.text1>
+      {{ $title }}
+    </x-atoms.texts.text1>
+  </x-atoms.containers.container1>
+</div>
