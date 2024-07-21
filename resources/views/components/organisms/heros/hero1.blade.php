1f9554653b1cb82d92fd4d903c5146698dd0fb46
dreamersvalera <valera@dreamersofday.co>
2024-07-21 12:16:47 +0300

    feat: add hero1

--- resources/views/components/organisms/heros/hero1.blade.php
+++ resources/views/components/organisms/heros/hero1.blade.php
@@ -1,7 +1,7 @@
 <div data-component="{{ $componentName }}" {{ $attributes->merge(['class' => '']) }}>
   <x-atoms.containers.container1>
-    <x-atoms.texts.text1>
+    <x-atoms.titles.title1 class="py-4">
       {{ $title }}
-    </x-atoms.texts.text1>
+    </x-atoms.titles.title1>
   </x-atoms.containers.container1>
 </div>
