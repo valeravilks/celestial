7cd8ea7cfe56ac08e6947241479b2d6d06a6b3c0
dreamersvalera <valera@dreamersofday.co>
2024-10-19 01:31:15 +0300

    feat: disable default colour tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,20 +6,20 @@ const config = {
 // add daisyUI plugin
   plugins: [typography],
   theme: {
+    colors: {
+      primary: '#0003ff',
+      secondary: '#C90000',
+      white: '#ffffff',
+      black: '#000000',
+      light: '#F6F5F5',
+      dark: '#361515',
+      gray: '#D9D9D9',
+      lightgray: '#BBBBBB',
+    },
     extend: {
       fontFamily: {
         sans: 'Open Sans, sans-serif',
       },
-      colors: {
-        primary: '#0003ff',
-        secondary: '#C90000',
-        white: '#ffffff',
-        black: '#000000',
-        light: '#F6F5F5',
-        dark: '#361515',
-        gray: '#D9D9D9',
-        lightgray: '#BBBBBB',
-      }, // Extend Tailwind's default colors
     },
   },
 };
