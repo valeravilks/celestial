43c434dd1418b59959ce658a64d08b78515ff384
Valera Vilks <valeravilks@gmail.com>
2025-08-02 20:53:40 +0300

    feat: add "Media Content 1" pattern to theme

---
+++ theme/patterns/media-content-1.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Title: Media Content 1
+ * Slug: celestial-fse-theme/media-content-1
+ * Categories: content
+ */
+
+?>
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|40","left":"var:preset|spacing|40"}}}} -->
+    <div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"","style":{"spacing":{"blockGap":"0"}}} -->
+        <div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"layout":{"type":"constrained","justifyContent":"center"}} -->
+            <div class="wp-block-group"><!-- wp:paragraph {"className":"is-style-paragraph-subtitle","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|20"}}}} -->
+                <p class="is-style-paragraph-subtitle" style="margin-bottom:var(--wp--preset--spacing--20)">Subtitle</p>
+                <!-- /wp:paragraph -->
+
+                <!-- wp:heading {"className":"is-style-paragraph-subtitle","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|30"}}}} -->
+                <h2 class="wp-block-heading is-style-paragraph-subtitle" style="padding-bottom:var(--wp--preset--spacing--30)">Heading</h2>
+                <!-- /wp:heading -->
+
+                <!-- wp:paragraph -->
+                <p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                <!-- /wp:paragraph --></div>
+            <!-- /wp:group --></div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"verticalAlignment":"center","width":""} -->
+        <div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"aspectRatio":"16/9","scale":"cover","className":"is-style-default"} -->
+            <figure class="wp-block-image is-style-default"><img src="https://cdn.stocksnap.io/img-thumbs/960w/F8IX8796HF.jpg" alt="" style="aspect-ratio:16/9;object-fit:cover"/></figure>
+            <!-- /wp:image --></div>
+        <!-- /wp:column --></div>
+    <!-- /wp:columns --></div>
+<!-- /wp:group -->
