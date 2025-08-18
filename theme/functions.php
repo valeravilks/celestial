7a1eb225c6f81d50f144e2f044e6ed42aaa7ded2
Valera Vilks <valeravilks@gmail.com>
2025-08-18 17:45:40 +0300

    feat: add "Arrow" style for button block with custom CSS and JSON configuration

--- theme/functions.php
+++ theme/functions.php
@@ -4,3 +4,13 @@
 add_action('init', function() {
     remove_theme_support( 'core-block-patterns' );
 });
+
+add_action( 'init', 'themeslug_enqueue_block_styles' );
+
+function themeslug_enqueue_block_styles() {
+    wp_enqueue_block_style( 'core/button', array(
+        'handle' => 'themeslug-block-image',
+        'src'    => get_theme_file_uri( "assets/blocks/core-button.css" ),
+        'path'   => get_theme_file_path( "assets/blocks/core-button.css" )
+    ) );
+}
