c88ff47bb03b6f1f75844b9184c93f29d84bab50
Valera Vilks <valeravilks@gmail.com>
2025-08-18 06:05:10 +0300

    feat: add custom gradient background for core image block

--- theme/functions.php
+++ theme/functions.php
@@ -8,6 +8,16 @@ add_action('init', function() {
 add_action( 'init', 'themeslug_enqueue_block_styles' );
 
 function themeslug_enqueue_block_styles() {
+    wp_enqueue_block_style( 'core/image', array(
+        'handle' => 'themeslug-block-image',
+        'src'    => get_theme_file_uri( "assets/blocks/core-image.css" ),
+        'path'   => get_theme_file_path( "assets/blocks/core-image.css" )
+    ) );
+}
+
+add_action( 'init', 'themeslug_enqueue_block_styles' );
+
+function themeslug_enqueue_block_styles() {
     wp_enqueue_block_style( 'core/button', array(
         'handle' => 'themeslug-block-image',
         'src'    => get_theme_file_uri( "assets/blocks/core-button.css" ),
