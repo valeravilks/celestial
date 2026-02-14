a1de2a8e1ca7407889981818a7992a9daf934a67
Valera Vilks <valeravilks@gmail.com>
2026-02-15 00:04:41 +0300

    Remove `cel-editor` package components and PHP script, clean up unused dependencies

--- cel-core/celestial-core-plugin.php
+++ cel-core/celestial-core-plugin.php
@@ -15,3 +15,24 @@ require_once __DIR__ . '/vendor/autoload.php';
 use ValeraVilks\Celestial\LandingPages\LandingPages;
 
 LandingPages::init();
+
+add_action('enqueue_block_editor_assets', function () {
+	$build_dir = __DIR__ . '/build/editor/';
+
+	if (
+		! file_exists($build_dir . '/index.js') ||
+		! file_exists($build_dir . '/index.asset.php')
+	) {
+		return;
+	}
+
+	$asset = require $build_dir . '/index.asset.php';
+
+	wp_enqueue_script(
+		'celestial-editor',
+		plugins_url('build/index.js', __FILE__),
+		$asset['dependencies'],
+		$asset['version'],
+		true
+	);
+});
