7f6bdbf4b2b9d2e1e96620034494242affb9bb31
Valera Vilks <valeravilks@gmail.com>
2026-02-18 11:07:45 +0300

    feat: add new example blocks (`example-dynamic`, `example-interactive`, `example-static`) with initial setup including PHP render files, JS, CSS, documentation, and configurations. Clean up and consolidate files for consistency.

--- cel-core/blocks/example-static/save.js
+++ cel-core/blocks/example-static/save.js
@@ -5,7 +5,6 @@
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
  */
 import { useBlockProps } from '@wordpress/block-editor';
-import { Icon } from './icon';
 
 /**
  * The save function defines the way in which the different attributes should
@@ -18,8 +17,8 @@ import { Icon } from './icon';
  */
 export default function save() {
 	return (
-		<button { ...useBlockProps.save() }>
-			<Icon />
-		</button>
+		<p { ...useBlockProps.save() }>
+			{ 'Example Static – hello from the saved content!' }
+		</p>
 	);
 }
